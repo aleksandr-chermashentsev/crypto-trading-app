8ad34dda54d55a0f9db8a8c9a6b46e1942c875ca
a.chermashentsev <a.chermashentsev@corp.mail.ru>
2021-11-02 08:42:48 +0300

    Add metric for update interval of candle

--- robot-impl/src/main/java/ru/avca/robot/InfoProvider.java
+++ robot-impl/src/main/java/ru/avca/robot/InfoProvider.java
@@ -4,16 +4,16 @@ import com.google.common.util.concurrent.AtomicDouble;
 import io.micrometer.core.instrument.MeterRegistry;
 import io.micrometer.core.instrument.Tag;
 import io.micronaut.context.annotation.Context;
-import io.micronaut.context.event.ApplicationEventPublisher;
 import io.micronaut.runtime.event.annotation.EventListener;
 import io.micronaut.scheduling.annotation.Async;
+import ru.avca.robot.event.CandlestickEvents;
 import ru.avca.robot.event.RobotEvents;
 
 import javax.inject.Inject;
 import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
 
 /**
  * @author a.chermashentsev
@@ -23,16 +23,34 @@ import java.util.concurrent.ScheduledExecutorService;
 public class InfoProvider {
 
     @Inject private MeterRegistry meterRegistry;
-    @Inject private ApplicationEventPublisher publisher;
-    private final ScheduledExecutorService scheduledExecutorService = Executors.newSingleThreadScheduledExecutor();
-    private final ConcurrentHashMap<String, AtomicDouble> gauges = new ConcurrentHashMap<>();
+    private final ConcurrentHashMap<String, AtomicDouble> athValueGauges = new ConcurrentHashMap<>();
+    private final ConcurrentHashMap<String, AtomicLong> candleStickLastUpdateTimes = new ConcurrentHashMap<>();
+    private final ConcurrentHashMap<String, AtomicInteger> candleStickGauges = new ConcurrentHashMap<>();
 
     @EventListener
     @Async
     public void onCloseToAthValues(RobotEvents.CloseToAthValues closeToAthValues) {
-        AtomicDouble atomicDouble = gauges.computeIfAbsent(closeToAthValues.getSymbol(), key -> meterRegistry.gauge("robot.close_to_ath", List.of(Tag.of("symbol", key)),
+        AtomicDouble atomicDouble = athValueGauges.computeIfAbsent(closeToAthValues.getSymbol(), key -> meterRegistry.gauge("robot.close_to_ath", List.of(Tag.of("symbol", key)),
                 new AtomicDouble()
         ));
         atomicDouble.set(closeToAthValues.getClosePercent());
+    }
+
+    @EventListener
+    @Async
+    public void onCandlestickEvent(CandlestickEvents.BinanceCandlestickEvent binanceCandlestickEvent) {
+        long currentTime = System.currentTimeMillis();
+        String symbol = binanceCandlestickEvent.getBinanceEvent().getSymbol();
+        AtomicLong lastUpdateTime = candleStickLastUpdateTimes.computeIfAbsent(symbol, key -> new AtomicLong());
+        long lastUpdateTimeUnboxed = lastUpdateTime.get();
+        if (lastUpdateTimeUnboxed > 0) {
+            AtomicInteger diffBetweenUpdateTime = candleStickGauges.computeIfAbsent(symbol,
+                    key -> meterRegistry.gauge("robot.candle_update_interval", List.of(Tag.of("symbol", symbol)), new AtomicInteger())
+            );
+
+            diffBetweenUpdateTime.set((int) (currentTime - lastUpdateTimeUnboxed));
+        }
+
+        lastUpdateTime.set(currentTime);
     }
 }
